7282bffcab0c8aa48f623ef18048f1f50d9cb4e6
Ryan Mohta <ryanmohta@gmail.com>
2021-04-09 12:53:56 -0700

    Added questions for yams

--- src/Yams/YamQuestionData.js
+++ src/Yams/YamQuestionData.js
@@ -1,102 +1,102 @@
 export const yamQuestionData = [
   {
-    question: "Question question question 1?",
+    question: "1. What kind of food is a yam?",
     answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
+      "Fruit",
+      "Vegetable",
+      "Juice",
+      "Water"
+    ],
+    correctAnswerIndex: 1
+  },
+  {
+    question: "2. How many species of yams are found around the world?",
+    answers: [
+      "20",
+      "50",
+      "400",
+      "600"
+    ],
+    correctAnswerIndex: 3
+  },
+  {
+    question: "3. Which continent produces the most yams?",
+    answers: [
+      "Africa",
+      "Asia",
+      "North America",
+      "UC Irvine"
+    ],
+    correctAnswerIndex: 0
+  },
+  {
+    question: "4. Which is these is NOT a possible color of the inside of a yam?",
+    answers: [
+      "White",
+      "Orange",
+      "Yellow",
+      "Purple"
+    ],
+    correctAnswerIndex: 1
+  },
+  {
+    question: "5. What season are yams usually harvested in?",
+    answers: [
+      "Spring",
+      "Summer",
+      "Fall",
+      "Winter"
     ],
     correctAnswerIndex: 2
   },
   {
-    question: "Question question question 2?",
+    question: "6. Which vitamin are yams NOT a major source of?",
     answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
+      "Vitamin A",
+      "Vitamin B",
+      "Vitamin C",
+      "Potassium"
+    ],
+    correctAnswerIndex: 0
+  },
+  {
+    question: "7. In Ghana, yams are traditionally served to celebrate what?",
+    answers: [
+      "AntAlmanac and Zotistics joining PeterPortal",
+      "Mars starting work at Cameo",
+      "Ryan stealing Patrick's Igloo",
+      "The arrival of a new baby"
+    ],
+    correctAnswerIndex: 3
+  },
+  {
+    question: "8. What food item are yams usually confused with?",
+    answers: [
+      "Bananas",
+      "Potatoes",
+      "Sweet Potatoes",
+      "Taro"
     ],
     correctAnswerIndex: 2
   },
   {
-    question: "Question question question 3?",
+    question: "9. For how long have people been cultivating yams?",
     answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
+      "2,000 years",
+      "10,000 years",
+      "100,000 years",
+      "Since the start of time"
     ],
-    correctAnswerIndex: 2
+    correctAnswerIndex: 1
   },
   {
-    question: "Question question question 4?",
+    question: "10. Which is NOT a health benefit of yams?",
     answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
+      "Heals skin diseases",
+      "Cures respiratory problems",
+      "Aids digestion",
+      "Improves eyesight"
     ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 5?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 6?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 7?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 8?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 9?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
-  },
-  {
-    question: "Question question question 10?",
-    answers: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ],
-    correctAnswerIndex: 2
+    correctAnswerIndex: 3
   },
 ];
